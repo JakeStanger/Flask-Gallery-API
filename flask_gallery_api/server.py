a199aa9a64ee913fdbc410c8b44df07d6305ac82
Jake Stanger <mail@jstanger.dev>
2019-09-23 20:21:25 +0100

    Add CORS headers after request

--- flask_gallery_api/server.py
+++ flask_gallery_api/server.py
@@ -106,6 +106,14 @@ def require_can_edit(func):
 # def index():
 #     return render_template('main.html', title='Home')
 
+@app.after_request
+def after_request(response):
+    response.headers.add('Access-Control-Allow-Origin', '*')
+    response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+    response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    return response
+
+
 @app.route('/user', methods=['GET'])
 @jwt_required
 def user():
